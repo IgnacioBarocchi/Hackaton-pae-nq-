eeec287e2d06e7f7fdf7f52c6c38bd359efb569e
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2020-11-11 22:27:02 -0300

    Add test of mutability of the sorting functions

---
+++ CLIENT/tests/dbSorters.test.ts
@@ -0,0 +1,20 @@
+import { COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE } from '../src/data/COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE';
+import { sortByLikes } from '../src/helpers/dbSorters';
+//@ts-ignore
+
+describe('Mustation', () => {
+  //@ts-ignore
+  const input1 = [...COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE];
+  const input2 = [...COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE];
+  //@ts-ignore
+  it('returns a sorted data base', () => {
+    //@ts-ignore
+    expect(sortByLikes(input1).length).toBe(
+      COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE.length
+    );
+    //@ts-ignore
+    expect(sortByLikes(input2).length).toBe(
+      COLLECTION_OF_IDEAS_FROM_HACKATHON_PAE.length
+    );
+  });
+});
